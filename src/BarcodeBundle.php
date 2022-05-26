dee04fa8b678ec03d8d564b5e1c3e27f86e364d9
Tac Tacelosky <tacman@gmail.com>
2022-05-26 13:24:19 -0400

    cleanup

--- src/BarcodeBundle.php
+++ src/BarcodeBundle.php
@@ -11,37 +11,22 @@ class BarcodeBundle extends AbstractBundle
 {
 
     // $config is the bundle Configuration that you usually process in ExtensionInterface::load() but already merged and processed
+    /**
+     * @param array<mixed> $config
+     */
     public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
     {
-//        $loader = new XmlFileLoader($builder, new \Symfony\Component\Config\FileLocator(__DIR__.'/Resources/config/'));
-//        $loader->load('services.xml');
-//        $definition = $builder->getDefinition('survos.Barcode');
-
-//        $serviceIdentifier = 'picqer.barcode';
-//        $definition = $builder->autowire($serviceIdentifier, Barcode::class);
-//        $container->services()->alias(Barcode::class, $serviceIdentifier);
-
         $definition = $builder->autowire('survos.barcode_twig', BarcodeTwigExtension::class)
             ->addTag('twig.extension');
 
         $definition->setArgument('$widthFactor', $config['widthFactor']);
         $definition->setArgument('$height', $config['height']);
         $definition->setArgument('$foregroundColor', $config['foregroundColor']);
-
-
-//        dd($config, $definition->getProperties(), $definition->getArguments());
-
-
-//
-//        <service id="Survos\BaseBundle\Twig\Extensions" public="true" autowire="true">
-//            <tag name="twig.extension" />
-//        </service>
-
     }
 
     public function configure(DefinitionConfigurator $definition): void
     {
-        // if the configuration is short, consider adding it in this class
+        // since the configuration is short, we can add it here
         $definition->rootNode()
             ->children()
             ->scalarNode('widthFactor')->defaultValue(2)->end()
